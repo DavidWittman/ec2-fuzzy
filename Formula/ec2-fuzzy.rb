63d329dfe6449df8c6bc850befb65bfe7651b4df
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-07 16:27:02 +0000

    Brew formula update for ec2-fuzzy version v0.0.2

--- Formula/ec2-fuzzy.rb
+++ Formula/ec2-fuzzy.rb
@@ -2,16 +2,16 @@
 class Ec2Fuzzy < Formula
   desc "Fuzzy search EC2 instances and SSH to them"
   homepage "https://github.com/DavidWittman/ec2-fuzzy"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/DavidWittman/ec2-fuzzy/releases/download/v0.0.1/ec2-fuzzy_0.0.1_Darwin_x86_64.tar.gz"
-    sha256 "72651e1b2282b940951ca8f3b949a5644c5b1e6d1e51dd35788942fd0d843b78"
+    url "https://github.com/DavidWittman/ec2-fuzzy/releases/download/v0.0.2/ec2-fuzzy_0.0.2_Darwin_x86_64.tar.gz"
+    sha256 "d7a6e2eb3f151dd4faf5021cb6e201e613270ff3cd7f935e8cdc7c066528874b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/DavidWittman/ec2-fuzzy/releases/download/v0.0.1/ec2-fuzzy_0.0.1_Linux_x86_64.tar.gz"
-    sha256 "e70f2a5c61cc864360615a56612177c2a5d351968259dd4e80f3463ff5177cc4"
+    url "https://github.com/DavidWittman/ec2-fuzzy/releases/download/v0.0.2/ec2-fuzzy_0.0.2_Linux_x86_64.tar.gz"
+    sha256 "5472adbbe6fa11325cb2cb9804750e4b5a994eff7b2da54f7519d6d665e57ee7"
   end
 
   def install
